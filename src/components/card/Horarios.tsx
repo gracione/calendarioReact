cf4d9bd6b8773fa463b4670ce4739bc4484d21b0
gracione <gracione2017@gmail.com>
2022-09-02 12:27:35 -0300

    corrigindo

--- src/components/card/Horarios.tsx
+++ src/components/card/Horarios.tsx
@@ -4,30 +4,43 @@ import api from './../../services/api';
 
 export default function Horarios(props: any) {
   const [token] = useState(localStorage.getItem('token'));
-  const [teste, setTest] = useState();
   
-  let horariosDisponivel: any[] = [];
+  let horariosDisponivel: string[] = [];
   let listaHorarios: any = [];
   api
     .post("/horarios-marcados", token)
     .then((response) => {
-      setTest(response.data);
-//      horariosDisponivel.push(response.data[0].horario_inicio);
-    }
-    );
-    
-  /////////////////
+      response.data.forEach((element: any) => {
+        horariosDisponivel[0] = element.horario_inicio;
+      });
+    });
 
-  console.log(teste);
-    
-  horariosDisponivel.forEach(element => {
-    listaHorarios.push(
-      <div className="horario-disponivel" >
-        <h4>{element}
-        </h4>
-        <img src="https://cdn-icons-png.flaticon.com/512/151/151770.png" alt="" />
-      </div>);
-  });
+    const [transactions, setTransactions] = useState([]);
+
+
+    async function createTransaction() {
+      let vetor: any = [];
+      const response = await api.post("/horarios-marcados", {
+      });
+      response.data.forEach((element: { horario_inicio: any; }) => {
+        vetor.push(element.horario_inicio);
+      });
+      setTransactions(vetor);
+    }
+    createTransaction();
+    console.log(transactions);
+//  horariosDisponivel.forEach((element: any) => {
+//    console.log(element);
+//  });
+//    horariosDisponivel.forEach(element => {
+//           listaHorarios.push(
+//             <div className="horario-disponivel" >
+//               <h4>{element}
+//               </h4>
+//               <img src="https://cdn-icons-png.flaticon.com/512/151/151770.png" alt="" />
+//             </div>);
+//    });
+//    
   return (
     <HorariosCard >
       {props.data}
