e9f96eab8e7bcfd63a1aa8c8e06bd24f3c8855b6
gracione <gracione2017@gmail.com>
2022-09-04 11:33:02 -0300

    await

--- src/components/card/Horarios.tsx
+++ src/components/card/Horarios.tsx
@@ -4,29 +4,26 @@ import api from './../../services/api';
 
 export default function Horarios(props: any) {
   const [token] = useState(localStorage.getItem('token'));
-  
+  const [transactions, setTransactions] = useState<[]>([]);
+
   let horariosDisponivel: string[] = [];
   let listaHorarios: any = [];
 
 
-    const response = api.post("/horarios-marcados", {});
-    console.log(response);
-//  horariosDisponivel.forEach((element: any) => {
-//    console.log(element);
-//  });
-//    horariosDisponivel.forEach(element => {
-//           listaHorarios.push(
-//             <div className="horario-disponivel" >
-//               <h4>{element}
-//               </h4>
-//               <img src="https://cdn-icons-png.flaticon.com/512/151/151770.png" alt="" />
-//             </div>);
-//    });
-//    
-  return (
-    <HorariosCard >
+    async function createTransaction() {
+      const response = await api.post("/horarios-marcados", {});
+        setTransactions(response.data[0].horario_inicio);
+        return response.data[0].horario_inicio;
+    }
+    
+    createTransaction();
+    console.log(transactions);
+
+return (
+  <HorariosCard >
       {props.data}
       {listaHorarios}
+      {transactions}
     </HorariosCard>
   );
 }
